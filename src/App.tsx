eef791aa14a0a2b6715d50f2013334aba463ed7d
L16-code <lavishjain2002@gmail.com>
2024-07-15 10:27:20 +0530

    added some features

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-// import './App.css'
+import { useLocation } from 'react-router-dom';
 import PublicRoutes from './routes/PublicRoutes'
-// import '../public/adminKit/js/app.js'
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 function App() {
+  const location = useLocation();
 
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+  }, [location]);
   return (
     <PublicRoutes />
   )
