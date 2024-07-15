5fe0bf938b314e7463ccebe7f9cfdb42f32ac6cf
L16-code <lavishjain2002@gmail.com>
2024-07-15 12:29:07 +0530

    added some features

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { useLocation } from 'react-router-dom';
 import PublicRoutes from './routes/PublicRoutes'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ReactGA from 'react-ga4';
+import axios from 'axios';
 function App() {
   const location = useLocation();
 
+  const [ip, setIp]=useState();
   useEffect(() => {
+    GetuserIp()
     ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
   }, [location]);
+  const GetuserIp=async()=>{
+    const ip = await axios.get('https://ipapi.co/json')
+    setIp(ip.data);
+    console.log(ip.data);
+  }
   return (
     <PublicRoutes />
   )
